e7d24cf031b7b08dffc506907758903731b537cc
NarumonAZ <122533887+NarumonAZ@users.noreply.github.com>
2023-04-03 10:40:09 +0700

    Add files via upload

---
+++ quiz2/code/Program.cs
@@ -0,0 +1,43 @@
+﻿class Program {
+    static void Main() {
+
+            int sum = 0;
+            int max = int.Max;
+            int min = int.Min;
+
+
+            while (true)
+            {
+                string name = Console.ReadLine();
+
+                if (name == "end")
+                {
+                    break;
+                }
+            }
+
+            if (true){
+                string nam = Console.ReadLine();
+
+                if (nam == "findmax")
+                {
+                    Console.WriteLine(a + max);
+                    break;
+                }
+                else if (nam == "findmin")
+                {
+                    Console.WriteLine(b + min);
+                    break;
+                }
+                else if (nam == "findmean")
+                {
+                    Console.WriteLine(c + (sum / count));
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("*");
+                }
+            }
+        }
+    }
